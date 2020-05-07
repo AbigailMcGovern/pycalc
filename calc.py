aa20229cb032f2aa6f144bef1d8077a28f302f1d
AbigailMcGovern <abigail_mcgovern@hotmail.com>
2020-05-07 15:14:53 +1000

    fixed intentional bug

--- calc.py
+++ calc.py
@@ -4,6 +4,7 @@ from time import sleep
 
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
+    num0, num1 = int(num0), int(num1)
     if operator == '+':
         return num0 + num1
     elif operator == '-':
@@ -12,6 +13,7 @@ def compute(expression):
         print('unknown operator!')
         return None
 
+        
 def until_the_cows_come_home():
     the_cows_are_home = False
     while not the_cows_are_home:
